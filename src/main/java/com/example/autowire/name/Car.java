860444059e2853fc17d14f3ea7878c0f589960eb
Gautam Gupta <ggautamg23@gmail.com>
2026-06-30 18:50:11 +0530

    Value Annotations

--- src/main/java/com/example/autowire/name/Car.java
+++ src/main/java/com/example/autowire/name/Car.java
@@ -1,13 +1,13 @@
 package com.example.autowire.name;
 
 public class Car {
-    private Specification specification;
+    private Specification specification1;
 
-    public void setSpecification(Specification specification) {
-        this.specification = specification;
+    public void setSpecification1(Specification specification) {
+        this.specification1 = specification;
     }
 
     public void displayDetails(){
-        System.out.println("Car Details : " + specification.toString());
+        System.out.println("Car Details : " + specification1.toString());
     }
 }
